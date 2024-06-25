2dcee8a64847646561be2842bfd04f9a35fde18f
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-06-25 23:44:11 +0200

    refactor(ui): update `Text` with design tokens

--- packages/ui/src/components/text/text.css.ts
+++ packages/ui/src/components/text/text.css.ts
@@ -1,32 +1,36 @@
 import { style } from "@vanilla-extract/css";
-import { fontTokens } from "../../tokens/font";
-
-// NOTE: Define tokens for all values
+import { tokens } from "../../tokens";
+import { lightTheme } from "../../theme/light.css";
+import { toPx } from "../../tokens/utils";
 
 export const h1 = style({
-  fontFamily: fontTokens.family.sans,
-  fontSize: fontTokens.size[110],
-  lineHeight: "40px",
-  fontWeight: fontTokens.weight.bold,
+  fontFamily: tokens.font.family.sans,
+  fontSize: tokens.font.size[110],
+  lineHeight: toPx(tokens.size["size-140"]),
+  fontWeight: tokens.font.weight.bold,
+  color: lightTheme.vars.text.heading,
 });
 
 export const h2 = style({
-  fontFamily: fontTokens.family.sans,
-  fontSize: fontTokens.size[70],
-  lineHeight: fontTokens.lineHeight[110],
-  fontWeight: fontTokens.weight.bold,
+  fontFamily: tokens.font.family.sans,
+  fontSize: tokens.font.size[70],
+  lineHeight: toPx(tokens.size["size-110"]),
+  fontWeight: tokens.font.weight.bold,
+  color: lightTheme.vars.text.heading,
 });
 
 export const h3 = style({
-  fontFamily: fontTokens.family.sans,
-  fontSize: fontTokens.size[60],
-  lineHeight: "28px",
-  fontWeight: fontTokens.weight.bold,
+  fontFamily: tokens.font.family.sans,
+  fontSize: tokens.font.size[60],
+  lineHeight: toPx(tokens.size["size-90"]),
+  fontWeight: tokens.font.weight.bold,
+  color: lightTheme.vars.text.heading,
 });
 
 export const h4 = style({
-  fontFamily: fontTokens.family.sans,
-  fontSize: fontTokens.size[50],
-  lineHeight: fontTokens.lineHeight[70],
-  fontWeight: fontTokens.weight.bold,
+  fontFamily: tokens.font.family.sans,
+  fontSize: tokens.font.size[50],
+  lineHeight: toPx(tokens.size["size-70"]),
+  fontWeight: tokens.font.weight.bold,
+  color: lightTheme.vars.text.heading,
 });
