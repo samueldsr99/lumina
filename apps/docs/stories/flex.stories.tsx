9678960f7af6d39a39a57188ddbe1e5d804f707e
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-06-26 21:33:26 +0200

    feat: add `Flex`

--- apps/docs/stories/flex.stories.tsx
+++ apps/docs/stories/flex.stories.tsx
@@ -1,19 +1,80 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { TextInput } from "@acme/ui";
+import { Box, Flex } from "@acme/ui";
+import { tokens } from "@acme/ui/tokens";
 
-const meta: Meta<typeof TextInput> = {
-  component: TextInput,
+const meta: Meta<typeof Flex> = {
+  component: Flex,
   title: "Layout/Flex",
+  args: {
+    container: true,
+  },
   argTypes: {
-    type: {},
+    container: {
+      control: {
+        type: "boolean",
+        disable: true,
+      },
+    },
+    orientation: {
+      control: {
+        type: "select",
+      },
+      options: ["horizontal", "vertical"],
+    },
+    gap: {
+      control: {
+        type: "select",
+      },
+      options: Object.keys(tokens.spacing),
+    },
+    justifyContent: {
+      control: {
+        type: "select",
+      },
+      options: ["flex-start", "center", "flex-end"],
+    },
+    alignItems: {
+      control: {
+        type: "select",
+      },
+      options: ["flex-start", "center", "flex-end"],
+    },
   },
 };
 
 export default meta;
 
-type Story = StoryObj<typeof TextInput>;
+type Story = StoryObj<typeof Flex>;
+
+function Item(): JSX.Element {
+  return (
+    <Box
+      sx={{
+        size: "size-200",
+        backgroundColor: "gray-200",
+        borderRadius: 40,
+      }}
+    />
+  );
+}
 
 export const Default: Story = {
-  render: (props) => <></>,
+  render: (props) => (
+    <Flex container {...props}>
+      <Flex>
+        <Item />
+      </Flex>
+      <Flex>
+        <Item />
+      </Flex>
+      <Flex>
+        <Item />
+      </Flex>
+    </Flex>
+  ),
   name: "Flex",
+  args: {
+    gap: "space-40",
+    orientation: "vertical",
+  },
 };
---
+++ packages/ui/src/components/flex/flex.tsx
@@ -0,0 +1,60 @@
+import type { SprinklesProperties } from "../../css/sprinkles.css";
+import { Box } from "../box";
+
+interface FlexContainerProps {
+  container: true;
+  children: React.ReactNode;
+  gap?: SprinklesProperties["gap"];
+  wrap?: boolean;
+  orientation?: "horizontal" | "vertical";
+  alignItems?: SprinklesProperties["alignItems"];
+  alignContent?: SprinklesProperties["alignContent"];
+  justifyContent?: SprinklesProperties["justifyContent"];
+}
+
+interface FlexItemProps {
+  container?: false;
+  children: React.ReactNode;
+  grow?: boolean;
+  shrink?: boolean;
+}
+
+export type FlexProps = FlexContainerProps | FlexItemProps;
+
+const isContainer = (props: FlexProps): props is FlexContainerProps =>
+  (props as FlexContainerProps).container;
+
+export function Flex(props: FlexProps) {
+  if (isContainer(props)) {
+    return (
+      <Box
+        as="div"
+        sx={{
+          display: "flex",
+          flexDirection: props.orientation === "horizontal" ? "row" : "column",
+          gap: props.gap,
+          ...(props.wrap ? { flexWrap: "wrap" } : {}),
+          ...(props.alignItems ? { alignItems: props.alignItems } : {}),
+          ...(props.alignContent ? { alignContent: props.alignContent } : {}),
+          ...(props.justifyContent
+            ? { justifyContent: props.justifyContent }
+            : {}),
+        }}
+      >
+        {props.children}
+      </Box>
+    );
+  }
+
+  return (
+    <Box
+      as="div"
+      sx={{
+        ...(props.grow ? { flexGrow: 1 } : {}),
+        ...(props.shrink ? { flexShrink: 1 } : {}),
+      }}
+    >
+      {props.children}
+    </Box>
+  );
+}
