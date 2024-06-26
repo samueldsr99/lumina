e84949f21b53dc3be700b51dc7063f9e3ae35e20
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-06-26 21:44:40 +0200

    chore: update box definition

--- packages/ui/src/components/box/box.tsx
+++ packages/ui/src/components/box/box.tsx
@@ -1,4 +1,6 @@
 import type { ElementType } from "react";
+import type { SprinklesProperties } from "../../css/sprinkles.css";
+import { sprinkles } from "../../css/sprinkles.css";
 
 interface AsProp<T extends ElementType> {
   as?: T;
@@ -7,10 +9,16 @@ interface AsProp<T extends ElementType> {
 type PropsToOmit<T extends ElementType, P> = keyof (AsProp<T> & P);
 
 export type BoxProps<T extends React.ElementType> = AsProp<T> &
-  Omit<React.ComponentPropsWithoutRef<T>, PropsToOmit<T, AsProp<T>>>;
+  Omit<React.ComponentPropsWithoutRef<T>, PropsToOmit<T, AsProp<T>>> & {
+    sx?: SprinklesProperties;
+  };
 
 export function Box<T extends ElementType>(props: BoxProps<T>): JSX.Element {
-  const { as: Tag = "div", children, ...rest } = props;
+  const { as: Tag = "div", children, sx, ...rest } = props;
 
-  return <Tag {...rest}>{children}</Tag>;
+  return (
+    <Tag className={sprinkles({ ...sx })} {...rest}>
+      {children}
+    </Tag>
+  );
 }
