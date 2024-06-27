d33220071c76672faf9204a564fa4b1f2cb41dc2
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-06-27 19:00:25 +0200

    fix(docs): import lumina styles

--- apps/docs/.storybook/preview.tsx
+++ apps/docs/.storybook/preview.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import type { Preview } from "@storybook/react";
 import { ThemeDecorator } from "../components/decorators";
 
+import "@lumina/ui/styles.css";
 import "../styles.css";
 
 const preview: Preview = {
